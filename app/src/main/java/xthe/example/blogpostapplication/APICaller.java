4f8512a992a370145227ad38db5dd37ad3c76b6e
SW <helenhe588@gmail.com>
2019-04-19 22:17:00 -0400

    Adding UI and activity for login and sign up page (No API connection yet)

---
+++ app/src/main/java/xthe/example/blogpostapplication/APICaller.java
@@ -0,0 +1,50 @@
+package xthe.example.blogpostapplication;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class APICaller extends AsyncTask<String, String, String> {
+
+    // Static values
+    private String connecter = "/";
+
+    // Constructor
+    public APICaller () {
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String urlString = params[0]; // URL to call
+        String data = params[1]; //data to post
+        OutputStream out = null;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            out.close();
+
+            urlConnection.connect();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return data;
+    }
+}
--- app/src/main/java/xthe/example/blogpostapplication/MainActivity.java
+++ app/src/main/java/xthe/example/blogpostapplication/MainActivity.java
@@ -1,13 +1,38 @@
 package xthe.example.blogpostapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected String authen_API_key;
+    protected String user_token = "";
+    private static final int REQUEST_SIGNLOGIN = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (user_token.isEmpty()){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivityForResult(intent, REQUEST_SIGNLOGIN);
+        }
+    }
+
+
+
+    // API URL and Message generators
+    private String generateURL() {
+        String URL = "";
+
+        return URL;
+    }
+
+    private String generateMessage() {
+        String message = "";
+
+        return message;
     }
 }
